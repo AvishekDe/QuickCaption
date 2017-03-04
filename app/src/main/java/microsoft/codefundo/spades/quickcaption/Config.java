d49b568971f45b6a295478fee3671293b82388e6
AvishekDe <avishekde.iitr@gmail.com>
2017-03-05 00:43:06 +0530

    Initial commit

---
+++ app/src/main/java/microsoft/codefundo/spades/quickcaption/Config.java
@@ -0,0 +1,13 @@
+package microsoft.codefundo.spades.quickcaption;
+
+/**
+ * Created by avishek on 3/4/17.
+ */
+
+public class Config {
+    // File upload url (replace the ip with your server address)
+    public static final String FILE_UPLOAD_URL = "http://10.42.0.152:5000/upload";
+
+    // Directory name to store captured images and videos
+    public static final String IMAGE_DIRECTORY_NAME = "Codefundo";
+}
